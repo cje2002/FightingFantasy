874f2f79ef54c8f39dd8c92e2eac0a6ecf196f1b
christian.ellis@gmail.com <christian.ellis@gmail.com>
2020-03-17 19:48:57 +0000

    Add project files.

---
+++ CheeseOnToast/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace FightingFantasy.Converters
+{
+    internal class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool && (bool)value)
+            {
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return (value is Visibility && (Visibility)value == Visibility.Visible);
+        }
+    }
+}
---
+++ CheeseOnToast/Models/CharacterModel.cs
@@ -0,0 +1,80 @@
+﻿using FightingFantasy.Commands;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace FightingFantasy.Models
+{
+    public class CharacterModel : INotifyPropertyChanged
+    {
+        private string name = "jsjsdjsiddjio";
+        private int luck;
+        private int stamina;
+        private int skill;
+
+        public string Name
+        {
+            get => name; 
+            set
+            {
+                name = value;
+                this.PropertyChanged(this, new PropertyChangedEventArgs("Name"));
+            }
+        }
+        public int Luck
+        {
+            get => luck; 
+            set
+            {
+                luck = value;
+                this.PropertyChanged(this, new PropertyChangedEventArgs("Luck"));
+            }
+        }
+        public int Stamina
+        {
+            get => stamina; 
+            set
+            {
+                stamina = value;
+                this.PropertyChanged(this, new PropertyChangedEventArgs("Stamina"));
+            }
+        }
+        public int Skill
+        {
+            get => skill; 
+            set
+            {
+                skill = value;
+                this.PropertyChanged(this, new PropertyChangedEventArgs("Skill"));
+            }
+        }
+
+        public ICommand GenerateCharacterCommand { get; set; }
+
+        public CharacterModel()
+        {
+            this.GenerateCharacterCommand = new RelayCommand(() =>
+            {
+                this.GenerateCharacter();
+            });
+         }
+
+        public void GenerateCharacter()
+        {
+            Random random = new Random();
+            this.Luck = random.Next(1, 6) + 6;
+            this.Skill = random.Next(1, 6) + 6;
+            this.Stamina = random.Next(1, 6) + 6;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            // Raise the PropertyChanged event, passing the name of the property whose value has changed.
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
